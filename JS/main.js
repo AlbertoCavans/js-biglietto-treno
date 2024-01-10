3544a6871b9403f4c3496c68d0014f27ef79d462
AlbertoCavans <alberto96cavani@gmail.com>
2024-01-10 17:18:27 +0100

    Feat JS - wrong data scenario

--- JS/main.js
+++ JS/main.js
@@ -15,26 +15,31 @@ console.log(priceOnlyByKm);
 
 /* discount */
 let discount;
+if (!isNaN(kmRoute) && !isNaN(clientAge)) {
+  /* Calculate discounts */
+  /*!!!!! Underage discount */
+  if (clientAge < 18) {
+    discount = (priceOnlyByKm / 100) * 20;
+    console.log(discount);
+  } else if (clientAge >= 18) {
+    /*!!!!! NO discount */
+    discount = (priceOnlyByKm / 100) * 40;
+    console.log(discount);
+  } else if (clientAge > 65) {
+    /*  !!!!! OVER65 discount */
+    discount = (priceOnlyByKm / 100) * 0;
+    console.log(discount);
+  }
 
-/* Calculate discounts */
-/*!!!!! Underage discount */
-if (clientAge < 18) {
-  discount = (priceOnlyByKm / 100) * 20;
-  console.log(discount);
-} else if (clientAge >= 18) {
-  /*!!!!! NO discount */
-  discount = (priceOnlyByKm / 100) * 40;
-  console.log(discount);
-} else if (clientAge > 65) {
-  /*  !!!!! OVER65 discount */
-  discount = (priceOnlyByKm / 100) * 0;
-  console.log(discount);
+  /* Final price */
+  const finalPrice = priceOnlyByKm - discount;
+
+  /* Print final price */
+  const labelPrice =
+    "Il tuo biglietto del treno costerà: " + finalPrice.toFixed(2) + "€";
+  resultElement.innerText = labelPrice;
+} else {
+  alert(
+    "Dati inseriti non corretti, inserire km con SOLO tasti numerici ed età SOLO con tasti numerici. Ricarica la pagina."
+  );
 }
-
-/* Final price */
-const finalPrice = priceOnlyByKm - discount;
-
-/* Print final price */
-const labelPrice =
-  "Il tuo biglietto del treno costerà: " + finalPrice.toFixed(2) + "€";
-resultElement.innerText = labelPrice;
